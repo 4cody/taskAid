267effe05dc14b02f720df247f814a9ed96b4a4a
Cody Shaner <cshaner04@gmail.com>
2020-09-23 08:25:47 -0500

    add backend

---
+++ src/app.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const express = require('express');
+require('./db/mongoose');
+const userRouter = require('./routers/user');
+const taskRouter = require('./routers/task');
+
+const app = express();
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Orgin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
+
+app.use('/api', userRouter);
+app.use('/api', taskRouter);
+
+//  serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('/../client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '/../client/build/index.html'));
+  });
+}
+
+module.exports = app;
