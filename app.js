ed05c0798a2dde88810329e8f49a6268bceda776
Cody Shaner <cshaner04@gmail.com>
2020-09-23 10:19:40 -0500

    remove src directory

--- app.js
+++ app.js
@@ -24,10 +24,10 @@ app.use('/api', userRouter);
 app.use('/api', taskRouter);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('/../client/build'));
+  app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '/../client/build/index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 app.get('/', (req, res) => {
